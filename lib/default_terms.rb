96493fd4c0167c2040fd0801ebe42b1c4e60ba57
A.B <a.b320012@gmail.com>
2018-05-23 10:37:17 -0400

    added file to commit

---
+++ lib/default_terms.rb
@@ -0,0 +1,121 @@
+DEFAULT_TERMS=    "<h1>DEALERS LIMITED SERVICE CONTRACT TERMS AND CONDITIONS</h1>
+
+  <h2>SILVER AND GOLD COVERED COMPONENTS AND DEFINITIONS</h2>
+  <p>
+  (THE FOLLOWING COMPONENTS ARE COVERED ONLY IF LISTED UNDER THE MAXIMUM PAYOUT PER COMPONENT SECTION ON THE PURCHASED PACKAGE. IF A COMPONENT IS LISTED BELOW BUT IS NOT LISTED
+  UNDERNEATH THE MAXIMUM PAYOUT PER COMPONENT SECTION ON THE PURCHASED PACKAGE, IT IS NOT COVERED.)
+
+  <ul>
+    <li>ENGINE (Diesel and Gas): All internally lubricated parts inside the engine block and cylinder head. The valve cover, oil pan, engine block, and cylinder heads are also covered if they are damaged by an internally lubricated part. The intake manifold, exhaust manifold, starter, thermostat, motor mounts, and water pump are also covered.</li>
+    <li>TRANSMISSION: All internally lubricated parts contained within the transmission case, as well as the transmission case itself if it is damaged by an internally lubricated part. External parts covered include the torque
+    converter, slave cylinder, flywheel, flex plate, transmission mounts and transmission pan.</li>
+    <li>TRANSFER CASE: All internally lubricated parts contained within the transfer case, along with the 4x4 shift/encoder motor and transfer case mounts. The transfer case is only covered if damaged by an internally
+    lubricated part.</li>
+    <li>DRIVE AXLES: All internally lubricated parts within the front and rear drive axles, as well as the axle housing if damaged by an internally lubricated part. The propeller shafts and universal joints are also covered.</li>
+    <li>COOLING SYSTEM: Only the Water Pump.</li>
+    <li>AC: The compressor is the only part covered on the 3 month or 3,000 mile or 6 month 6,000 mile service contracts. Recharging the AC is NOT covered.</li>
+    <li>STARTER: Only the Starter.</li>
+    <li>ALTERNATOR: Only the Alternator.</li>
+    <li>FUEL PUMP: Only the Fuel Pump.</li>
+    <li>TURBO: Only the Factory Installed Turbo and Interior Parts.</li>
+    <li>ELECTRICAL: Voltage regualtor, starter motor and driver, starter solenoid, front windshield wiper motor, rear wiper motor, front/rear windshield washer pump(s), delay circuit board, relay and switch. Electronic ignition module, body control module, distributor, engine management control computer, spark control/anti-detonation sensor and controller. Power window motors and power window regulator, power door lock actuators, heated back glass-element, wiring harness, horn(s), manually operated electrical switches (except audio/video control switches). Controlling power door locks and windows, headlight switch, turn signal switch, wiper switch, cruise control engagement switch. Cruise control module, servo and transducer.</li>
+  </ul>
+
+  <p>
+    Seals and Gaskets, related to covered parts, are covered on vehicles up to 10 years old with less than 100,000 miles. The timing belt is covered on vehicles up to 10 years old with less than 100,000 miles.
+  </p>
+
+  <p>
+    <strong>All FSA service contracts begin same day of purchase.</strong>
+  </p>
+
+  <h3>VEHICLE OPERATOR INSTRUCTIONS</h3>
+  <ul>
+    <li>Always monitor vehicle temperature or pressure warning systems and make sure they are working properly.</li>
+    <li>Check the level and condition of all coolants, lubricants, and fluids routinely.</li>
+    <li>Turn the vehicle OFF immediately at the sign of any mechanical failure and call roadside assistance and the administrator before taking the vehicle to any repair facility.</li>
+    <li>Drive your vehicle within the factory tolerances, failures determined to have occurred due to abuse or negligence will be denied.</li>
+    <li>Perform required manufacturer recommended maintinence.</li>
+  </ul>
+
+  <h3>WHAT IS NOT COVERED</h3>
+  <ul>
+    <li>Any repair made without an Authorization Number and an authorized dollar amount.</li>
+    <li>Any damaged part that is not listed under the covered parts section or if damage occurs to a covered part in result of damage to a non-covered/non-listed part.</li>
+    <li>Any part or repair made due to overheating, regardless of cause, that is caused by improper fluids or lubricants as well as insufficient levels and/or any contamination of said fluids or lubricants.</li>
+    <li>Any repair intended to repair normal wear and tear on parts that have not been damaged. Parts that have worn beyond the factory tolerances resulting in smoking, compression loss, consumption of oil, engine</li>
+    knocks or valve train ticks.</li>
+    <li>Damage to parts that could have been avoided by ceasing operation of an already damaged vehicle.</li>
+    <li>Any damage caused by non-factory modifications, extreme off road use, racing or competitive motor sports.</li>
+    <li>Damage in direct result of abuse or utilizing the vehicle outside of factory towing and load suggestions.</li>
+    <li>Part failure caused by corrosion, rust, carbon build-up, varnish or sludge.</li>
+    <li>Any mechanical failure that is covered by the factory, factory-recall or any other form of coverage.</li>
+    <li>Part failure due to any kind of impact, wreck, theft, water, vandalism, insects, vermin, riot, or war.</li>
+    <li>Expense due to injury, storage, loss of time, inconvenience.</li>
+    <li>All shop supplies, disposal fees, diagnosis, taxes and shop labor rates exceeding $95 an hour.</li>
+    <li>Any part failures deemed by the administrator to have consequential damages or pre-existing issues.</li>
+    <li>Parts that have not failed completely.</li>
+  </ul>
+
+  <h3>WHAT TO DO IF YOUR VEHICLE BREAKS DOWN</h3>
+
+  <ul>
+    <li>If vehicle shows signs of or has a mechanical breakdown, take immediate steps to ensure that the vehicle isn’t further damaged. CAUTION-Any damage occurring from the continued use of a vehicle with a mechanical breakdown will NOT be covered.</li>
+    <li>If vehicle is located within 50 miles of the selling dealer and if the selling dealer has a repair facility, safely deliver vehicle to said dealer. If you’re located outside 50 miles you may deliver your vehicle to a Licensed Repair Facility of your choice.</li>
+    <li>Ensure that the repair facility contacts the First Secured Administrators LLC claims department prior to any repairs. First Secure Administrators LLC claims department can be reached at 1-833-FSA-PLAN.</li>
+  </ul>
+
+  <p>
+    <strong>Actions of the Administrator when a claim is reported</strong> - It is the job of the Administrator to confirm the mechanical failure with the repair facility, confirm coverage of damaged parts or components, determine the
+    appropriate cost of the repair, and authorize the claim for any covered repairs. The administrator has the sole discretion to replace covered parts with new parts, re-manufactured parts, or with used parts of like
+    kind and quality. The Administer maintains the right to inspect your vehicle and to confirm any failure(s). In the case that the Administrator determines that the repair is not a valid claim, or that the broken parts are
+    not covered under the covered parts list, then you are responsible to pay for those repairs.
+  </p>
+
+  <p>
+    <strong>Cancellation/Transfer of Warranty</strong> - The original service contract holder is allowed to return the service contract within (20) days of the date the service contract was mailed to the service contract holder or within
+    ten (10) days of delivery if the service contract is delivered to the service contract holder at the time of the vehicle sale or within a longer time period permitted under the service contract. No claim can be made
+    under the service contract prior to its return to the dealer. Once the service contract is returned and voided, the dealer shall refund the full purchase price of the service contract to the service contract holder, or
+    credit the account of the service contract holder. If a refund is not paid or credited within (45) days after the return of the service contract, a ten percent (10%) penalty per month shall be added to the refund credit.
+    The Administrator of this Service Contract may cancel this Contract with written notice to the Contract Holder’s last known address with at least 30 days’ notice of such cancellation for the following reasons: (1)
+    material misrepresentation related to the Vehicle; (2) substantial change in the risk assumed, unless the Administrator has reasonably foreseen the change or contemplated the risk when entering into this Service
+    Contract; (3) extensive breach of duties, conditions, or warranties by the Contract Holder relating to the Vehicle: (4) suspension or revocation of the Contract Holder’s driver License. A 10 day notice will be given for
+    non-payment cancellations. The Finance Company/Lien Holder may cancel this service contract on your behalf in the event your vehicle is repossessed or your vehicle is declared a total loss. The finance company
+    is also to be listed as a joint payee and to receive any refund if this contract is canceled. This service contract is transferable within 10 days of the change in ownership. To transfer the remaining coverage of your
+    vehicle to the new owner, you must contact the administrator. There will be a $100.00 transfer fee. If you sell your vehicle or if there is any change in ownership of your vehicle, and you don’t wish to transfer the
+    service contract, this service contract will be terminated.
+  </p>
+
+  <p>
+    <strong>Definitions: Administrator</strong> - First Secured Administrators LLC (FSA), 2173 S. 1640 W. Suite F, Woods Cross, Utah 84087 - 1-833-FSA-Plan. Obligator/WE/Our: The Selling Automobile Dealer Identified on the first
+    page of this contract from whom you purchased this service contract. Licensed Repair Facility: Any automotive repair facility that has been licensed to perform automotive repairs by the State in which it operates.
+    Service Contract: this document in its entirety, that explains the coverage and limitations provided to you.
+  </p>
+
+  <p>
+    <strong>eneral Provisions</strong> - All service contracts can only be purchased at the time of the vehicle sale by the selling Dealer. A signed copy of the service contract must be submitted by the The Selling Dealer/Obligator to
+    the Administrator (FSA) within 36 hours of the vehicle purchase. The selling Dealer agrees that the “Obligations of the dealer under this service contract are backed by the full faith and credit of the dealer”. The
+    selling Dealer/Obligator of this service contract shall mail a written notice to the service contract holder at the last known address of the service contract holder contained in the records of the provider at least
+    (10) days prior to cancellation by the dealer. Prior notice is not required if the reason for cancellation is nonpayment of the dealer fee, a material misrepresentation by the service contract holder to the dealer or a
+    substantial breach of duties by the service contract holder relating to the covered product or its use.
+  </p>
+
+  <p>
+    <strong>Utah</strong> - This service is subject to limited regulation by the Utah Insurance Department. To file a complaint, contact the Utah Insurance Department. Coverage afforded under this service contract is not guaranteed by
+    the Utah Property and Causality Association. This Service contract purchase price is payable, in full, at the time of purchase.
+  </p>
+
+  <p>
+    “EMERGENCY REPAIR”: Repairs, which if not performed promptly, will cause extensive problems for your vehicle and/or render your vehicle as unsafe to drive.
+    If a emergency repair on covered parts is needed outside the administrators business hours to prevent further damage, safely deliver your Vehicle to a Licensed Repair Facility and have the diagnostic completed at
+    a reasonable and customary rate. Then, during the Administrators regular business hours, report the diagnostic to the Administrator. Repair authorization will be allowed only during regular business hours. Regular
+    business hours are Monday through Friday 8am to 6pm MT except major holidays.
+  </p>
+
+  <p>
+    <strong>Arbitration</strong> - You and the Dealer/Obligator hereby agree that in the event of any dispute arising under this agreement that cannot be settled by informal means, the parties agree to submit the dispute to binding
+    arbitration according to the Utah Arbitration Act and the Commercial Arbitration Rules of the American Arbitration Association. All decisions as to the scope, effect and enforceability of this arbitration agreement
+    shall be decided by the arbitrator, not by the court. The award rendered by the arbitrator may be entered in any and or all courts having jurisdiction. You (buyer) nor the Dealer/Obligator shall file or maintain any
+    lawsuit in any court against the other and agree that any suit filed in violation of their Service Contract shall be dismissed by the court in favor of an arbitration conducted pursuant to this Vehicle Service Contract.
+    The prevailing party shall be entitled to collect from the other party all costs incurred in the dispute, including reasonable attorneys fees. By signing the front of this contract, each party understands that any
+    arbitration arising from this Service Contract is final and binding, and each party is waiving certain rights. Including but not limited to the right to litigate their dispute in court, including the right to a jury trial.</p>"
