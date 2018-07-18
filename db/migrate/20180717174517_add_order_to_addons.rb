1403d4a6bc73142413d5b4137f288db2ef043e27
A.B <a.b320012@gmail.com>
2018-07-18 13:39:05 -0400

    fixing addon migrate

--- db/migrate/20180717174517_add_order_to_addons.rb
+++ db/migrate/20180717174517_add_order_to_addons.rb
@@ -1,13 +1,5 @@
 class AddOrderToAddons < ActiveRecord::Migration[5.1]
   def change
     add_column :addons, :order, :integer
-    Package.all.each{|p|
-      i = 0
-      p.addons.each{|a|
-        a.order = i
-        i += 1
-        a.save
-      }
-    }
   end
 end
