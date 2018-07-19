a77dc6c16745e73308ab21dc3760c7d1ed021647
A.B <a.b320012@gmail.com>
2018-07-19 13:23:15 -0400

    removed absolute mileage constraint

--- app/models/package.rb
+++ app/models/package.rb
@@ -2,7 +2,7 @@ class Package < ApplicationRecord
   has_paper_trail meta: { template_id: :template_id, dealership_id: ->(pkg) { pkg.dealership_id if pkg.template  } }
 
   validates :name, presence: true
-  validates :absolute_mileage, inclusion: { in: [ true, false ] }
+  # validates :absolute_mileage, inclusion: { in: [ true, false ] }
 
   belongs_to :template
   delegate :dealership_id,:dealership, to: :template
